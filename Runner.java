f2f69e28e7b3c244d8350813b0dfeb5d7ebef84d
EddieResendizRRHS <73211323+EddieResendizRRHS@users.noreply.github.com>
2020-10-30 09:56:50 -0500

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,11 @@
+import kareltherobot.*; 
+
+public class Runner
+{
+  public static void main(String[] args)
+  {
+    World.setVisible(true); 
+    World.setDelay(50);
+    //World.readWorld("introWorld.kwld");
+  }
+}
